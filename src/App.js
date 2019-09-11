c447b7a4e92d96857969e219107cf0b6f90bb204
Devyn Gish <devyngish@gmail.com>
2019-09-11 09:17:40 -0600

    hello

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Container, Header, } from "semantic-ui-react"
+import FlashCard from "./flashers/FlashCard.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  state = { 
+    flashCards: [
+      { id: 1, flashFront: "what is 2+2?", flashBack: "4"},
+      { id: 2, flashFront: "what color is Devyn's necklace", flashBack: "Red" },
+      { id: 3, flashFront: "What is 5x5?", flashBack: "25" },
+    ],
+  }
+
+
+render() {
+  return(
+    <Container style={{ paddingTop: "30px"}}>
+      <Header as="h2">
+         Flash cards!!
+         <FlashCard flashcards  = {this.state.flashCards} />
+      </Header>
+     </Container>
+
+   )
+  } 
 }
 
 export default App;
